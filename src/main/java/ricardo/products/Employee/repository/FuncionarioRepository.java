d76caca67025c9fb827dd44e1cbbbe60a4d94479
Ricardo Neto <ricardofukushimaneto@gmail.com>
2025-07-16 16:17:47 -0300

    password

--- src/main/java/ricardo/products/Employee/repository/FuncionarioRepository.java
+++ src/main/java/ricardo/products/Employee/repository/FuncionarioRepository.java
@@ -3,5 +3,5 @@ package ricardo.products.Employee.repository;
 import ricardo.products.Employee.entity.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface   FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 }
